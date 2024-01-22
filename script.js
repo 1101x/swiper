2a4bb98ede6ff0744901a938b6e6337672cefb1f
1101x <junggg14@gmail.com>
2024-01-22 13:29:45 +0900

    알럿 수정

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 기본 코드
 
-const swiper = new Swiper(".swiper", {
-  effect: "cards",
+const swiper = new Swiper('.swiper', {
+  effect: 'cards',
   grabCursor: true,
   centeredSlides: true,
   loop: true,
@@ -10,21 +10,21 @@ const swiper = new Swiper(".swiper", {
 });
 
 //pc사이즈
-swiper.on("click", function () {
-  const msg = matchMedia("screen and (min-width: 768px)").matches
-    ? "팝업을 허용하여 카드를 다운받으시겠사와요?"
-    : "팝업을 허용하여\n카드를 다운받으시겠사와요?";
+swiper.on('click', function () {
+  const msg = matchMedia('screen and (min-width: 768px)').matches
+    ? '팝업을 허용하여 카드를 다운받으시겠사와요?\nSave the card.'
+    : '팝업을 허용하여\n카드를 다운받으시겠사와요?\nSave the card.';
 
   if (confirm(msg)) {
     const clickedNum = this.clickedSlide
-      .getAttribute("data-hash")
-      .replace("slide", "");
+      .getAttribute('data-hash')
+      .replace('slide', '');
     window.open(`./images/img_${clickedNum}.jpg`);
   }
 });
 
 /////새로고침 순서 바꾸기
-const swiperWrapper = document.querySelector(".swiper-wrapper");
+const swiperWrapper = document.querySelector('.swiper-wrapper');
 
 function newShuffle() {
   const cards = Array.from(swiperWrapper.children);
@@ -43,13 +43,13 @@ function newShuffle() {
 }
 
 // 새로고침버튼
-const shuffleButton = document.querySelector(".shuffle-btn");
-shuffleButton.addEventListener("click", function () {
+const shuffleButton = document.querySelector('.shuffle-btn');
+shuffleButton.addEventListener('click', function () {
   // location.reload();
   newShuffle();
 
   document.ontouchstart = () => {
-    shuffleButton.classList.add("touched");
-    setTimeout(() => shuffleButton.classList.remove("touched"), 500);
+    shuffleButton.classList.add('touched');
+    setTimeout(() => shuffleButton.classList.remove('touched'), 500);
   };
 });
